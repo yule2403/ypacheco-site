6bc23817addd8e3f6c725bb21d1fac7e2d9b64ef
YULEXIS <yule2403@gmail.com>
2015-03-29 18:20:59 -0400

    Advanced Javascript Lesson 10
    Lesson 10

--- js/Functions.js
+++ js/Functions.js
@@ -1,10 +1,37 @@
 // JavaScript Document
 
-function toggle(element) {
-  var e = document.getElementById(element);
-  if(e.style.display == "block") {
-    e.style.display = "none";
-  } else {
-    e.style.display = "block";
-  }
-};
+
+(function (document, window, undefined) {
+  'use strict';
+  
+  // Find each tooltip
+  var tooltip = $('.tooltip');
+
+  tooltip.each(function() {
+    // Create tooltip element
+    var $this = $(this),
+        tooltipText = $('<div class="tooltip-text">' + $this.data('tooltip-text') + '</div>');
+      
+    // Add tooltip to body on mouse over
+    $this.on('mouseover', function() {
+        tooltipText.appendTo('body');
+    });
+  
+    // Remove tooltip on mouseout
+    $this.on('mouseout', function() {
+        tooltipText.remove();
+    });
+  
+    // Attach the tooltip to the mouse cursor
+    $this.on('mousemove', function(e) {
+        tooltipText.css('top', (e.pageY + 20) + 'px');
+        tooltipText.css('left', (e.pageX + 20) + 'px');
+    });
+  
+  });
+  
+})(document, window);
+
+$('#name').tooltip({'trigger':'focus', 'title': "What's your name?"});
+$('#email').tooltip({'trigger':'focus', 'title': "What's your email?"});
+$('#comment').tooltip({'trigger':'focus', 'title': "What's up?"});
